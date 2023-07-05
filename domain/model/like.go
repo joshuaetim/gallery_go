e6cb6d4aee14bbe11a2957353904aa7807c9144d
Joshua Etim <jetimworks@gmail.com>
2023-07-05 16:13:29 +0100

    update constraint

--- domain/model/like.go
+++ domain/model/like.go
@@ -5,7 +5,7 @@ import "gorm.io/gorm"
 type Like struct {
 	gorm.Model
 	UserID  uint  `json:"userId"`
-	User    User  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:DELETE" json:"user"`
+	User    User  `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"user"`
 	PhotoID uint  `json:"photoId"`
-	Photo   Photo `gorm:"constraint:OnUpdate:CASCADE,OnDelete:DELETE" json:"photo"`
+	Photo   Photo `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE" json:"photo"`
 }
--- domain/repository/db/init.go
+++ domain/repository/db/init.go
@@ -39,6 +39,6 @@ func DB() *gorm.DB {
 		return nil
 	}
 
-	db.AutoMigrate(&model.User{})
+	db.AutoMigrate(&model.User{}, &model.Photo{}, &model.Like{})
 	return db
 }
